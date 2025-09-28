da5df3616044b22cf61c74388267e9b07a8d9d82
Wiszu23 <wiszo2306@gmail.com>
2025-09-28 18:42:38 +0200

    kosmetyka

--- shared/constants/network_constants.py
+++ shared/constants/network_constants.py
@@ -1,26 +1,19 @@
 class NetworkConstants:
     
-    CHAIN_ID_BSC = 56
     CHAIN_ID_ETH = 1
+    CHAIN_ID_BSC = 56
     CHAIN_ID_BASE = 8453
     
-    API_URL_BSC = "https://api.etherscan.io/v2/api?chainid=56"
     API_URL_ETH = "https://api.etherscan.io/v2/api?chainid=1" 
+    API_URL_BSC = "https://api.etherscan.io/v2/api?chainid=56"
     API_URL_BASE = "https://api.etherscan.io/v2/api?chainid=8453"
     
     WETH_ADDRESS_ETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-    WETH_ADDRESS_BASE = "0x4200000000000000000000000000000000000006"
     WBNB_ADDRESS_BSC = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"
+    WETH_ADDRESS_BASE = "0x4200000000000000000000000000000000000006"
+
     
     NETWORKS = {
-        "BSC": {
-            "api_url": API_URL_BSC,
-            "chain_id": CHAIN_ID_BSC,
-            "native_token_name": "BNB",
-            "native_token_full_name": "binancecoin",
-            "native_address": WBNB_ADDRESS_BSC,
-            "explorer": "https://bscscan.com"
-        },
         "ETH": {
             "api_url": API_URL_ETH,
             "chain_id": CHAIN_ID_ETH,
@@ -28,6 +21,14 @@ class NetworkConstants:
             "native_token_full_name": "ethereum",
             "native_address": WETH_ADDRESS_ETH,
             "explorer": "https://etherscan.io"
+        },
+        "BSC": {
+            "api_url": API_URL_BSC,
+            "chain_id": CHAIN_ID_BSC,
+            "native_token_name": "BNB",
+            "native_token_full_name": "binancecoin",
+            "native_address": WBNB_ADDRESS_BSC,
+            "explorer": "https://bscscan.com"
         },
         "BASE": {
             "api_url": API_URL_BASE,
